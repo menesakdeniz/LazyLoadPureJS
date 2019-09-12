467c4bacf1c49f0d08076c4659dec4e1bba0c7a8
Mustafa Enes AKDENİZ <140202080@kocaeli.edu.tr>
2019-09-12 21:11:51 +0300

    Create lazyload.js

---
+++ lazyload.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+	window.onscroll = function() {OnScrollMovement();};
+	setTimeout(function(){OnScrollMovement();},500);
+});
+function OnScrollMovement(){
+	var LazyLoadArray = document.getElementsByClassName("lazyload");
+	Array.from(LazyLoadArray).forEach((element) => {
+		var ElemClass = element.className.replace("lazyload","");
+		var ElemSRC = element.getAttribute("src");
+		var ElemDataSRC = element.getAttribute("data-src");
+		if(((ElemSRC != null && ElemSRC.length == 0) || ElemSRC == null) && ElemDataSRC != null&& ElemDataSRC.length > 0){
+			if(element.getBoundingClientRect().top <= window.innerHeight){
+				element.className = ElemClass;
+				element.setAttribute("src",ElemDataSRC);
+			}
+		}
+	});
+}
